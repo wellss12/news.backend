5f6130052b3c3c03792d549656cc91b64eaee05a
Wells <ll0918900@gmail.com>
2022-12-11 16:22:59 +0800

    Init Architecture

---
+++ NewsSystem.Tests/Application/NewsTest.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace NewsSystem.Tests.Application;
+
+[TestFixture]
+public class NewsTest
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Success_Create_News()
+    {
+        Assert.Pass();
+    }
+}
